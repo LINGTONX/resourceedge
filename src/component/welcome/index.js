4af6cf72c8a99a8f8173562948043ae3ab3c96dd
genesis <ambroselington@gmail.com>
2021-12-12 22:33:04 +0100

    updating changes
    changes updated

--- src/component/welcome/index.js
+++ src/component/welcome/index.js
@@ -3,17 +3,26 @@ import "./welcome.css"
 const Welcome = () => {
   return (
     <section className='welcome layout'>
-      <div>
+      {/* <div>
         <div>
           <span>Throw paperwork into the trash where it belongs.</span>
           <span>Eliminate all the hassle involved in managing people operations by automating it.</span>
 
         </div>
-        <div>
+        <div >
           <img src='images/relax.svg' />
         </div>
 
-      </div>
+      </div> */}
+
+      <div className="welcome-side-1 side padd-1rem">
+            <h3>Throw paperwork  into the trash where it belongs.</h3>
+            <span>Eliminate all the hassle involved in managing  people operations by automating it.</span>
+        </div>
+        <div className="welcome-side-2 side">
+             <img src='images/relax.svg'/>
+        </div>
+
     </section>
   )
 }
