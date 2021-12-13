af03bfcf69063ac5522ba3fbc78f85153ed7b982
genesis <ambroselington@gmail.com>
2021-12-13 02:09:19 +0100

    update: testing CI/CD with netlify

--- src/component/welcome/index.js
+++ src/component/welcome/index.js
@@ -4,20 +4,8 @@ import relax from "../../assest/images/relax.svg"
 const Welcome = () => {
   return (
     <section className='welcome layout'>
-      {/* <div>
-        <div>
-          <span>Throw paperwork into the trash where it belongs.</span>
-          <span>Eliminate all the hassle involved in managing people operations by automating it.</span>
-
-        </div>
-        <div >
-          <img src='images/relax.svg' />
-        </div>
-
-      </div> */}
-
       <div className="welcome-side-1 side padd-1rem">
-        <h3>Throw paperwork  into the trash where it belongs.</h3>
+        <h3>Bastard.</h3>
         <span>Eliminate all the hassle involved in managing  people operations by automating it.</span>
       </div>
       <div className="welcome-side-2 side">
